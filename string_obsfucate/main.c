72625a662d80a6f32dbbf451e514642e4dbc6abc
yakra13 <joshziebarth@yahoo.com>
2025-04-08 15:58:40 -0700

    asdfasd

---
+++ string_obsfucate/main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    FILE* f = fopen(argv[0]);
+    if(!f)
+    {
+
+    }
+
+    // find a "
+    // ftell its position
+    // extract to the next "
+    // encrypt with argv[1]
+    // replace in file
+    // write the file
+}
